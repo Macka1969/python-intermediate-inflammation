136ebd9aa3d9856a95965c314d40a3b2d7bf722e
stma <stma@noc.ac.uk>
2025-01-16 17:00:01 +0000

    changes

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -7,15 +7,10 @@ from inflammation import models, views
 
 
 def main(args):
-    """The MVC Controller of the patient inflammation data system.
 
-    The Controller is responsible for:
-    - selecting the necessary models and views for the current task
-    - passing data between models and views
-    """
     InFiles = args.infiles
     if not isinstance(InFiles, list):
-        InFiles = [args.infiles]
+        InFiles=[args.infiles]
 
 
     for filename in InFiles:
